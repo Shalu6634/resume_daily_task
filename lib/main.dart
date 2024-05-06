89caef1bebe336b03602200b496c5610d3375666
R4 <sharmashalu3148@>
2024-05-06 15:49:32 +0530

    6-05-24

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'package:resume_daily_task/utils/routes.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(statusBarColor: Colors.red),
+    );
+    SystemChrome.setPreferredOrientations([DeviceOrientation.landscapeLeft],);
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: AppRoutes.routes,
+    );
+  }
+}
---
+++ lib/utils/routes.dart
@@ -0,0 +1,8 @@
+
+import 'package:flutter/material.dart';
+import 'package:resume_daily_task/screens/home/homepage.dart';
+class AppRoutes
+{
+  static Map<String, Widget Function(BuildContext)> routes = {'/':(context)=>HomePage(),
+  };
+}
