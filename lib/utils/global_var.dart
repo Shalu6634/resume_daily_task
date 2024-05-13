59b2969dca778baad41fdd9b2046f40a86c27d6a
Shalu sharma <149373622+Shalu6634@users.noreply.github.com>
2024-05-13 15:19:03 +0530

    Update global_var.dart

--- lib/utils/global_var.dart
+++ lib/utils/global_var.dart
@@ -1,3 +1,53 @@
 
+
+import 'package:flutter/material.dart';
+
 String email = 'not';
 String pass='not';
+
+
+bool checkCapitalCase = false;
+bool checkLowerCase = false;
+
+int checkCharacter=0;
+bool checkDigit =false;
+
+int index=0;
+
+ List icon =[
+
+  Icons.inbox,
+  Icons.inbox_sharp,
+  Icons.person,
+  Icons.star_border_outlined,
+  Icons.snooze,
+  Icons.label_important_outline_sharp,
+  Icons.send,
+  Icons.schedule_send_outlined,
+  Icons.drafts,
+  Icons.email_outlined,
+   Icons.send,
+   Icons.schedule_send_outlined,
+   Icons.drafts,
+   Icons.email_outlined,
+];
+
+List  txt =[
+
+  'All inbox',
+  'Primary',
+  'Social',
+  'Starred',
+  'Snoozed',
+  'Important',
+  'Sent',
+  'Scheduled',
+  'Drafts',
+  'All email',
+  'Spam',
+  'Bin',
+  'Create new',
+  'Setting',
+];
+
+
