b1d220118c6788f913ba33b59572ed4a413ae3cf
Shalu sharma <149373622+Shalu6634@users.noreply.github.com>
2024-05-13 15:19:44 +0530

    Update drawerPage.dart

--- lib/screens/home/drawer/drawerPage.dart
+++ lib/screens/home/drawer/drawerPage.dart
@@ -1,4 +1,7 @@
+
 import 'package:flutter/material.dart';
+
+import '../../../utils/global_var.dart';
 
 class DrawerPage extends StatefulWidget {
   const DrawerPage({super.key});
@@ -15,177 +18,43 @@ class _DrawerPageState extends State<DrawerPage> {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              Padding(
-                padding: const EdgeInsets.only(right: 210, top: 40),
+             const  Padding(
+                padding: EdgeInsets.only(right: 210, top: 40),
                 child: Text(
                   'Gmail',
                   style: TextStyle(
-                      color: Colors.red,
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold,
-                     ),
-                ),
-              ),
-              ListTile(
-                leading: Icon(Icons.image_outlined),
-                title: Text(
-                  'All inbboxes',
-                  style:
-                      TextStyle(color: Colors.black,
-                          fontWeight: FontWeight.bold,
-                        letterSpacing: 1
-                      ),
-                ),
-              ),
-              ListTile(
-                leading: Icon(Icons.inbox),
-                title: Text(
-                  'Primary',
-                  style:
-                  TextStyle(color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                      letterSpacing: 1
+                    color: Colors.red,
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
               ),
-              ListTile(
-                leading: Icon(Icons.person),
-                title: Text(
-                  'Social',
-                  style:
-                  TextStyle(color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                      letterSpacing: 1
-                  ),
-                ),
+             const  Divider(
+                thickness: 2,
+                color: Colors.black,
               ),
-              ListTile(
-                leading: Icon(Icons.star_border_outlined),
-                title: Text(
-                  'Starred',
-                  style:
-                  TextStyle(color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                      letterSpacing: 1
-                  ),
-                ),
+              ...List.generate(
+                icon.length,
+                (index) => buildListTile(index),
               ),
-              ListTile(
-                leading: Icon(Icons.snooze),
-                title: Text(
-                  'Snoozed',
-                  style:
-                  TextStyle(color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                      letterSpacing: 1
-                  ),
-                ),
-              ),
-              ListTile(
-                leading: Icon(Icons.label_important_outline),
-                title: Text(
-                  'Important',
-                  style:
-                  TextStyle(color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                      letterSpacing: 1
-                  ),
-                ),
-              ),
-              ListTile(
-                leading: Icon(Icons.send),
-                title: Text(
-                  'Sent',
-                  style:
-                  TextStyle(color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                      letterSpacing: 1
-                  ),
-                ),
-              ),
-              ListTile(
-                leading: Icon(Icons.schedule_send_outlined),
-                title: Text(
-                  'Scheduled',
-                  style:
-                  TextStyle(color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                      letterSpacing: 1
-                  ),
-                ),
-              ),
-              ListTile(
-                leading: Icon(Icons.drafts),
-                title: Text(
-                  'Drafts',
-                  style:
-                  TextStyle(color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                      letterSpacing: 1
-                  ),
-                ),
-              ),
-              ListTile(
-                leading: Icon(Icons.email_outlined),
-                title: Text(
-                  'All email',
-                  style:
-                  TextStyle(color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                      letterSpacing: 1
-                  ),
-                ),
-              ),
-              ListTile(
-                leading: Icon(Icons.crisis_alert_sharp),
-                title: Text(
-                  'Spam',
-                  style:
-                  TextStyle(color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                      letterSpacing: 1
-                  ),
-                ),
-              ),
-              ListTile(
-                leading: Icon(Icons.delete_sweep_rounded),
-                title: Text(
-                  'Bin',
-                  style:
-                  TextStyle(color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                      letterSpacing: 1
-                  ),
-                ),
-              ),
-              ListTile(
-                leading: Icon(Icons.add),
-                title: Text(
-                  'Create new',
-                  style:
-                  TextStyle(color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                      letterSpacing: 1
-                  ),
-                ),
-              ),
-              ListTile(
-                leading: Icon(Icons.settings),
-                title: Text(
-                  'Setting',
-                  style:
-                  TextStyle(color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                      letterSpacing: 1
-                  ),
-                ),
-              ),
+
             ],
-          
           ),
         ),
       ),
-      appBar: AppBar(),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 4,
+      ),
+    );
+  }
+
+  Widget buildListTile(int index) {
+    return ListTile(
+
+      leading: Icon(icon[index]),
+      title: Text(txt[index]),
+
     );
   }
 }
